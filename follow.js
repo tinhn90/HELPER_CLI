b8de0a83e7fc3f286e2e0d94250da5410258d7f6
tindkhoang <hoangnhutin@gmail.com>
2024-04-30 19:51:00 +0700

    Update follow.js

--- follow.js
+++ follow.js
@@ -4,5 +4,5 @@ let followButtons = buttons.filter(button => button.textContent === 'Follow');
 followButtons.forEach((button, index) => {
     setTimeout(() => {
         button.click();
-    }, index * 2000);
+    }, index * 5000);
 });
