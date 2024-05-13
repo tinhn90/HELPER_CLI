c2cd526d375ce273c8e671ac0882fe0958436dd9
tindkhoang <hoangnhutin@gmail.com>
2024-05-13 09:23:49 +0700

    Update follow.js

--- follow.js
+++ follow.js
@@ -1,8 +1,17 @@
-let buttons = Array.from(document.querySelectorAll('button'));
-let followButtons = buttons.filter(button => button.textContent === 'Follow');
+//random 1-5 seconds
+let randomTime = Math.floor(Math.random() * 5 + 1) * 2000;
+//get all buttons
+let buttons = Array.from(document.querySelectorAll("button"));
+let followButtons = buttons.filter((button) => button.textContent === "Follow");
 
 followButtons.forEach((button, index) => {
-    setTimeout(() => {
-        button.click();
-    }, index * 5000);
+  setTimeout(() => {
+    button.click();
+  }, index * randomTime);
 });
+
+//scroll down 100px every 1-5 seconds
+let scrollDown = () => {
+  window.scrollBy(0, 200);
+};
+setInterval(scrollDown, Math.floor(Math.random() * 5 + 1) * 2000);
